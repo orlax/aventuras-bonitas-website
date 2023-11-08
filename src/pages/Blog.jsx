09758315f08a8c25cab48d08e0551f2d6543ae93
Orlando Almario <orlax22@gmail.com>
2023-11-08 15:22:19 -0500

    removed unused var

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,7 +1,5 @@
 import { Header } from "../components/Header"
 
-const video = require("../videos/gameplay_video.mp4")
-
 export function BlogScreen (){
     return(
     <div className="full-header">

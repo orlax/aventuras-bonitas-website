81410a05cc45f36601639e181307e5a5c3fea5e5
ch1spolo <polidavis28@gmail.com>
2025-02-28 08:48:37 -0500

    redirect presskit page to external URL using useEffect

--- src/app/[lang]/presskit/page.tsx
+++ src/app/[lang]/presskit/page.tsx
@@ -2,7 +2,11 @@
 import React, { useEffect } from "react";
 
 const Presskit = () => {
-    return window.location.href = "https://orlagames.notion.site/ab-presskit"; 
+  useEffect(() => {
+    window.location.href = "https://orlagames.notion.site/ab-presskit";
+  }, []);
+
+  return <></>;
 };
 
 export default Presskit;
